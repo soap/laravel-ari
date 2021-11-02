e991da64153b20cd80a129522674cf5ceb27bf70
soap <soap@users.noreply.github.com>
2021-11-02 17:28:23 +0000

    Fix styling

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,8 +4,8 @@ it('can test', function () {
     expect(true)->toBeTrue();
 });
 
-it('can use configuration values', function() {
+it('can use configuration values', function () {
     config()->set('ari.asterisk_host', '127.0.0.1');
-    
+
     expect(config('ari.asterisk_host'))->toBeString('127.0.0.1');
 });
